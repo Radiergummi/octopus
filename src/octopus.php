02935443bfe3a4983639ce90577df2cdcdc563fb
Mo <Radiergummi@users.noreply.github.com>
2015-07-03 13:46:26 +0200

    Update octopus.php

--- src/octopus.php
+++ src/octopus.php
@@ -7,6 +7,29 @@ class Octopus
 {
 	/**
 	 * query
+	 * holds the search query term
+	 * 
+	 * (default value: '')
+	 * 
+	 * @var string
+	 * @access public
+	 */
+	public $query = '';
+	
+	/**
+	 * results
+	 * holds the search results
+	 * 
+	 * (default value: array())
+	 * 
+	 * @var array
+	 * @access public
+	 */
+	public $results = array();
+
+	/**
+	 * filesToSearch
+	 * holds all files to search in
 	 * 
 	 * (default value: '')
 	 * 
@@ -14,40 +37,19 @@ class Octopus
 	 * @access public
 	 * @static
 	 */
-	public static $query = '';
+	public static $filesToSearch = array();
 
 	/**
-	 * directory
+	 * buildUrl
+	 * holds the callback for building URLs to your search results
 	 * 
-	 * (default value: '')
+	 * (default value: null)
 	 * 
-	 * @var string
+	 * @var Callable
 	 * @access public
 	 * @static
 	 */
-	public static $directory = '';
-
-	/**
-	 * files
-	 * 
-	 * (default value: array())
-	 * 
-	 * @var array
-	 * @access public
-	 * @static
-	 */
-	public static $files = array();
-
-	/**
-	 * searchpath
-	 * 
-	 * (default value: array())
-	 * 
-	 * @var array
-	 * @access public
-	 * @static
-	 */
-	public static $searchpath = array();
+	public static $buildUrl = null;
 
 	/**
 	 * excludes
@@ -59,17 +61,6 @@ class Octopus
 	 * @static
 	 */
 	public static $excludes = array();
-	
-	/**
-	 * results
-	 * 
-	 * (default value: array())
-	 * 
-	 * @var array
-	 * @access public
-	 * @static
-	 */
-	public static $results = array();
 
 	/**
 	 * surroundingTextLength
@@ -94,31 +85,63 @@ class Octopus
 	public static $resultsPerFile = 0;
 
 	/**
-	 * setup function.
+	 * __construct function.
 	 * setup settings for the search. You should load these from your configuration here,
-	 * for example using my project Libview @ github.com/Radiergummi/libview :)
+	 * for example using my project Libconfig @ github.com/Radiergummi/libconfig :)
+	 *459 087 893 hnq564
+	 * @example $results = new Search('foo')->set('path', '/content/sites')->find();
+	 * 
+	 * @access public
+	 * @param string $path (default: '')
+	 * @param mixed $excludes (default: [])
+	 * @param int $surroundingTextLength (default: 0)
+	 * @param int $resultsPerFile (default: 0)
+	 * @return $this
+	 */
+	public function __construct(
+		$query,
+		$path = '/path/to/search',
+		$excludes = ['file1.php', 'file2.php'],
+		$surroundingTextLength = 0,
+		$resultsPerFile = 0,
+		$buildUrl = null
+	) {
+		// the path to search in.
+		if (empty(static::$path)) static::$path = $path;
+		
+		// files to explictly exclude from searching
+		if (empty(static::$excludes)) static::$excludes = $excludes;
+		
+		// the amount of words around the search term to include in result snippets
+		if (empty(static::$surroundingTextLength)) static::$surroundingTextLength = $surroundingTextLength;
+		
+		// the amount of results per file to be regarded
+		if (empty(static::$resultsPerFile)) static::$resultsPerFile = $resultsPerFile;
+
+		// the list of files to search in
+		if (empty(static::$filesToSearch)) static::$filesToSearch = static::getFiles();
+		
+		if (empty($buildUrl)) static::$buildUrl = function($file) {
+			// add filepath relative to base search directory
+			$relativePath = substr($file->getPath() . DIRECTORY_SEPARATOR . $file->getFileName(), strlen(static::$searchpath));
+
+			return '/' . substr($relativePath, 0, -strlen($file->getExtension()));
+		};
+		
+		return $this;
+	}
+	
+	/**
+	 * set function.
+	 * method for setting default options for searches
 	 * 
 	 * @access public
 	 * @static
-	 * @param string $path (default: '')
-	 * @param mixed $excludes (default: [])
-	 * @param int $surrounding (default: 0)
-	 * @param int $results (default: 0)
-	 * @param string $extension (default: '')
-	 * @return void
+	 * @param string $key  the option name
+	 * @param mixed $value  the options value
 	 */
-	public static function setup(
-		$path = '',
-		$excludes = [],
-		$surrounding = 0,
-		$results = 0,
-		$extension = ''
-	) {
-		// the path to search in.
-		static::$searchpath = (! empty($path) ? $path : '/path/to/search');
-		static::$excludes = (! empty($excludes) ? $excludes : ['file1.php', 'file2.php']);
-		static::$surroundingTextLength = (! empty($surrounding) ? $surrounding : 10);
-		static::$resultsPerFile = (! empty($results) ? $results : 0);
+	public static function set($key, $value) {
+		static::$$key = $value;
 	}
 
 	/**
@@ -126,42 +149,29 @@ class Octopus
 	 * attempts to find a string in a directory and returns an array of results.
 	 * 
 	 * @access public
-	 * @static
 	 * @param mixed $query
 	 * @return array
 	 */
-	public static function find($query)
+	public function find()
 	{
-		// setup the configuration parameterss, if not done already
-		if (empty(static::$searchpath)) static::setup();
-		
-		// set query to class var
-		static::$query = $query;
-		
-		// iterate over specified folder, skipping . and .. directories
-		foreach(new RecursiveIteratorIterator(
-			new RecursiveDirectoryIterator(
-				static::$searchpath,
-				RecursiveDirectoryIterator::SKIP_DOTS
-			)
-		) as $file) {
+		foreach(static::$filesToSearch as $file) {
+
 			// skip excluded file
 			if (in_array($file->getFileName(), static::$excludes)) continue;
 
-			// add filepathpath relative to base search directory
-			static::$files[] = substr($file->getPath() . DS . $file->getFileName(), strlen(static::$searchpath));
 			
 			// search file for term
 			if (stristr($content = strip_tags(nl2br(file_get_contents($file)), '<br><code><p>'), static::$query) !== false) {
 
-				// build URL to resource
-				$result['url'] = '/' . substr(end(static::$files), 0, -strlen(EXT));
+				// call the callback for URL generation
+				//$result['url'] = '/' . substr(end(static::$files), 0, -strlen(EXT));
+				$result['url'] = call_user_func(static::buildUrl, $relativePath);
 				
 				// build name for resource
 				$result['title'] = ucwords(str_replace('-', ' ', end(@explode('/', Config::get('app.uri')))));
 			
 				// generate snippet with search term
-				if (preg_match_all('/((\s\S*){0,' . static::$surroundingTextLength . '})(' . static::$query . ')((\s?\S*){0,' . static::$surroundingTextLength . '})/im', $content, $matches, PREG_SET_ORDER)) {
+				if (preg_match_all('/((\s\S*){0,' . static::$surroundingTextLength . '})(' . $this->query . ')((\s?\S*){0,' . static::$surroundingTextLength . '})/im', $content, $matches, PREG_SET_ORDER)) {
 					// limit results per file
 					$resultLimit = (static::$resultsPerFile === 0 ? count($matches) : static::$resultsPerFile);
 
@@ -175,10 +185,41 @@ class Octopus
 				}
 
 				// if found, append to results
-				static::$results[] = $result;
+				$this->results[] = $result;
 			}
 		}
 
-		return static::$results;
+		return $this->results;
+	}
+	
+	/**
+	 * getFiles function.
+	 * retrieves all files within the speciefied directories as SPLFileInfo objects
+	 *
+	 * @access public
+	 * @static
+	 * @param array $directories  the directory paths to search
+	 * @return array  the retrieved files
+	 */
+	public static function getFiles($directories)
+	{
+		$files = array();
+		
+		// iterate over each directory in the array
+		foreach ((array) $directories as $directory) {
+
+			// create a new recursive directory iterator for a path, skipping "." and ".."
+			$directoryIterator = new RecursiveDirectoryIterator(
+				$directory,
+				RecursiveDirectoryIterator::SKIP_DOTS
+			);
+			
+			// add each found file to the array of files in which to search for the query.
+			foreach (new RecursiveIteratorIterator($directoryIterator) as $file) {
+				$files[] = $file;
+			}
+		}
+		
+		return $files;
 	}
 }
