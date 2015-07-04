31d50bed48288a5e16ccc21e11b7a3997bc73516
Mo <Radiergummi@users.noreply.github.com>
2015-07-04 15:00:21 +0200

    Create file2.php

---
+++ tests/fixtures/content/file2.php
@@ -0,0 +1,10 @@
+<div class="content" data-useless="more stuff">
+  <p>
+    Für Designer, Schriftsetzer, Layouter, Grafikenthusiasten und alle anderen.
+    Ein Projekt startet und doch es gibt noch keinen Text, allerdings sollte das Layout schon bald präsentiert werden ...
+    was tun?
+    Damit das Projekt gleich starten kann benutze einfach etwas Lorem ipsum - Blind-, Füll-, Dummy-, Nachahmungs-, 
+    Platzhaltertext. Generiere einfach soviel Lorem Ipsum Text wie du brauchst, kopiere und füge ihn in dein Layout 
+    als vorübergehenden Platzhalter ein. Somit sieht das Projekt ein Stückchen vollständiger aus als zuvor. Viel Spaß dabei.
+  </p>
+</div>
